90c58385a71b0c66c269840aaaf45f00f42f8db5
rbd3 <rabedesanaa@gmail.com>
2023-08-27 19:14:12 +0300

    Fix linters error

--- person.rb
+++ person.rb
@@ -1,41 +1,43 @@
 class Person
-  def initialize(name = "Unknown", age, parent_permission = true)
+  def initialize(name = 'Unknown', age, parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
-   end
-   
-   def get_id()
+  end
+
+  def get_id()
     @id
-   end
+  end
 
-   def get_name()
-    @name    
-    end
+  def get_name()
+    @name
+  end
 
-    def get_age()
-     @age
-    end
+  def get_age()
+    @age
+  end
 
-    def set_id(id)
-      @id = id
-    end
+  def set_id(id)
+    @id = id
+  end
 
-    def set_name(name)
-      @name = name
-    end
+  def set_name(name)
+    @name = name
+  end
 
-    def set_age(age)
-      @age = age
-    end
-    
-    private
-    def of_age?
-      @age >= 18
-    end
+  def set_age(age)
+    @age = age
+  end
 
-    public
-    def can_use_services?
-      @age >= 18 || @parent_permission == true
-    end
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    @age >= 18 || @parent_permission == true
+  end
 end
