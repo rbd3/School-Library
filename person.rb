34e35718eb471e08560124235e2d5510d420bccf
rbd3 <rabedesanaa@gmail.com>
2023-08-28 17:17:28 +0300

    Fix linters error

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
-  def initialize(age, parent_permission: true, name = 'Unknown')
-    @id = rand(1...1000)
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
