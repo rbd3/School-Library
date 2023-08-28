69b2aabc305e96a33b8b0f97edc5ba6a9bfd299b
rbd3 <rabedesanaa@gmail.com>
2023-08-28 17:11:52 +0300

    Update constructor

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Student < Person
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
-    super(name, age, parent_permission)
+    super(age, parent_permission, name)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(age, specialization, parent_permission: true, name: 'Unknown')
-    super(name, age, parent_permission)
+    super(age, parent_permission, name)
     @specialization = specialization
   end
 
