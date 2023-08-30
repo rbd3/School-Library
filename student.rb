32a5bb47f1c2f08667dbb9b82c857c4bd413c329
rbd3 <rabedesanaa@gmail.com>
2023-08-30 11:58:51 +0300

    Fix student class

--- student.rb
+++ student.rb
@@ -1,15 +1,20 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_accessor :name
+  attr_reader :classroom
 
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
-    classroom&.add_student(self)
   end
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
